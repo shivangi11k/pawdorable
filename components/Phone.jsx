a8a4844c5b0823e0ae54255885954c604bdc1710
Shivangi Kumari <144112791+shivangi11k@users.noreply.github.com>
2025-09-22 05:19:40 +0530

    feat(ui): Develop main Hero section

---
+++ components/Phone.jsx
@@ -0,0 +1,52 @@
+import React from 'react'
+
+const Phone = () => {
+  return (
+    <svg width="324" height="578" viewBox="0 0 324 578" fill="none" xmlns="http://www.w3.org/2000/svg">
+      <g filter="url(#filter0_b_190_1156)">
+        <path d="M5 22C5 10.9543 13.9543 2 25 2L299 2C310.046 2 319 10.9543 319 22V567C319 578.046 310.046 587 299 587H25C13.9543 587 5 578.046 5 567L5 22Z" fill="url(#paint0_linear_190_1156)" fillOpacity="0.8" />
+        <path d="M25 1.5C13.6782 1.5 4.5 10.6782 4.5 22L4.5 567C4.5 578.322 13.6782 587.5 25 587.5H299C310.322 587.5 319.5 578.322 319.5 567V22C319.5 10.6782 310.322 1.5 299 1.5L25 1.5Z" stroke="url(#paint1_radial_190_1156)" />
+      </g>
+      <g filter="url(#filter1_b_190_1156)">
+        <path d="M314 77V180.5L7 373.5V256.5L314 77Z" fill="white" fillOpacity="0.45" />
+      </g>
+      <g filter="url(#filter2_b_190_1156)">
+        <path d="M314 199V227.5L7 425V390L314 199Z" fill="white" fillOpacity="0.45" />
+      </g>
+      <rect x="9" y="4" width="306" height="539" rx="21" stroke="#323232" strokeWidth="8" />
+      <rect x="313" y="71" width="11" height="46" fill="#323232" />
+      <rect y="82" width="11" height="46" fill="#323232" />
+      <rect y="137" width="13" height="46" fill="#323232" />
+      <defs>
+        <filter id="filter0_b_190_1156" x="2" y="-1" width="320" height="591" filterUnits="userSpaceOnUse" colorInterpolationFilters="sRGB">
+          <feFlood floodOpacity="0" result="BackgroundImageFix" />
+          <feGaussianBlur in="BackgroundImage" stdDeviation="1" />
+          <feComposite in2="SourceAlpha" operator="in" result="effect1_backgroundBlur_190_1156" />
+          <feBlend mode="normal" in="SourceGraphic" in2="effect1_backgroundBlur_190_1156" result="shape" />
+        </filter>
+        <filter id="filter1_b_190_1156" x="6" y="76" width="309" height="298.5" filterUnits="userSpaceOnUse" colorInterpolationFilters="sRGB">
+          <feFlood floodOpacity="0" result="BackgroundImageFix" />
+          <feGaussianBlur in="BackgroundImage" stdDeviation="0.5" />
+          <feComposite in2="SourceAlpha" operator="in" result="effect1_backgroundBlur_190_1156" />
+          <feBlend mode="normal" in="SourceGraphic" in2="effect1_backgroundBlur_190_1156" result="shape" />
+        </filter>
+        <filter id="filter2_b_190_1156" x="6" y="198" width="309" height="228" filterUnits="userSpaceOnUse" colorInterpolationFilters="sRGB">
+          <feFlood floodOpacity="0" result="BackgroundImageFix" />
+          <feGaussianBlur in="BackgroundImage" stdDeviation="0.5" />
+          <feComposite in2="SourceAlpha" operator="in" result="effect1_backgroundBlur_190_1156" />
+          <feBlend mode="normal" in="SourceGraphic" in2="effect1_backgroundBlur_190_1156" result="shape" />
+        </filter>
+        <linearGradient id="paint0_linear_190_1156" x1="5" y1="2" x2="475.53" y2="108.322" gradientUnits="userSpaceOnUse">
+          <stop stopColor="white" stopOpacity="0.47" />
+          <stop offset="1" stopColor="white" stopOpacity="0" />
+        </linearGradient>
+        <radialGradient id="paint1_radial_190_1156" cx="0" cy="0" r="1" gradientUnits="userSpaceOnUse" gradientTransform="translate(5 2) rotate(64.1735) scale(789.594 1049.52)">
+          <stop stopColor="white" stopOpacity="0.7" />
+          <stop offset="1" stopColor="white" stopOpacity="0" />
+        </radialGradient>
+      </defs>
+    </svg>
+  )
+}
+
+export default Phone
